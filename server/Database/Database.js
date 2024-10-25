db63935cbfebf208dca15a793fa64fa116dab7fd
Payyavula Mukesh <payyavulamukesh@gmail.com>
2024-10-25 16:02:37 +0530

    first commit

---
+++ server/Database/Database.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://payyavulamukesh:Pmy%4052141@crud.j5qda.mongodb.net/crud");
+        console.log("MongoDB connected Successfully");
+        
+    }catch (error){
+        console.log("MongoDB connection error");
+    }
+}
+
+module.exports = {mongoose, connectDB}
